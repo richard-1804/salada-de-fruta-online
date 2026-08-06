6f8840c76d109efe16d2c043ea3ba3d30c884f9b
Daniel Richard <danielrichard.vs@gmail.com>
2026-08-06 08:20:52 -0300

    Atualizações de CSS e HTML

---
+++ script.js
@@ -0,0 +1,11 @@
+import "./services/firebase.js";
+
+import "./js/app.js";
+import "./js/ui.js";
+import "./js/rooms.js";
+import "./js/game.js";
+import "./js/vote.js";
+import "./js/score.js";
+import "./js/timer.js";
+import "./js/chat.js";
+import "./js/categories.js";
